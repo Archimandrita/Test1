de88252e2956e872005e10f15d93a292eba39c72
Archimandrita <mchristodoulides@gmail.com>
2015-03-20 17:56:03 +0200

    A1

---
+++ F1/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+//#r "C:/Windows/Microsoft.NET/Framework/v4.0.30319/System.Windows.Forms.dll"
+open System.Windows.Forms
+open System.Drawing
+//C:\Program Files\DevExpress 14.1\Components\Bin\Framework
+ 
+let form = new Form()
+
+form.Text <- "sdd"
+
+form.BackColor <- Color.Beige
+
+let b = new Windows.Forms.Button()
+b.Text <- "press me"
+
+type x = Form
+     
+let f = new x()
+//test
+ 
+f.Show()
+
+
+form.Controls.Add(b)  
+
+form.ShowDialog() |> ignore
